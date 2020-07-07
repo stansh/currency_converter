48c60542130ebff7a54460118ffc90ea240a9771
Stan S <stan.shm@gmail.com>
2020-07-07 01:28:11 -0500

    conversion from USD to others works, some CSS

--- script.js
+++ script.js
@@ -3,35 +3,46 @@ const buttons = () => {
     fetch ('https://open.exchangerate-api.com/v6/latest')
      .then(res => res.json()) 
      .then(res=> {
-         const units = Object.keys(res.rates);
+         const units = Object.entries(res.rates);
          console.log(units);
-         for (const key of units) 
+         for (const [key,value] of units) 
              {
              let el = document.createElement('button');
              el.innerHTML = key;
-             el.value = key;
+             el.name = key;
+             el.value = value;
              el.setAttribute('class', 'button');
-             el.addEventListener('click', chooseCurrency)
+             
+             el.addEventListener('click', convert);
              document.body.appendChild(el);
              /* console.log(el) */
              }         
      })
  }
 
-const chooseCurrency = (e) => {
-      let clickedUnit = e.target;
-      console.log(clickedUnit.value);
-      return clickedUnit.value;
 
- }
+ const convert = (e) => {
+     let clickedUnit = e.target;
+     console.log(clickedUnit.value);
+     let unitValue = clickedUnit.value;
+     let fromAmount = document.querySelector('#fromAmount').value;
+     let error = document.querySelector('#error');
+     
+     if (!isNaN(fromAmount)) {
+        document.querySelector('#toAmount').value = (fromAmount * unitValue).toFixed(2);
+     } else {
+        error.innerHTML = 'Please enter a numeric value';
+     }
+     
+}
 
- const convert = () => {
     
-    let fromAmount = document.querySelector('#fromAmount').value;
+    /* let fromAmount = document.querySelector('#fromAmount').value;
     fetch ('https://open.exchangerate-api.com/v6/latest')
     .then(res => res.json())
     .then(res => {
-        /* const clickedUnit = chooseCurrency; */
+      
+        
         console.log(chooseCurrency());
         data = res.rates; 
         const ratio = data[chooseCurrency(e)]
@@ -39,8 +50,18 @@ const chooseCurrency = (e) => {
         console.log(ratio);
         document.querySelector('#toAmount').value = fromAmount * ratio.toFixed(2);      
         })
+ */
+
+
+/* const chooseCurrency = (e) => {
+    let clickedUnit = e.target;
+    console.log(clickedUnit.value);
+    return clickedUnit.value;
 
 }
+ */
+
+ 
 
 
 
