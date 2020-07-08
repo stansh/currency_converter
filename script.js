9fece984102a64f1f80666a25ec709d0f5bcdf0e
Stan S <stan.shm@gmail.com>
2020-07-08 01:24:37 -0500

    more CSS/Bootstrap

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
- 
+
+/* const data = require('currencyCodes.json') */
+
 const buttons = () => {
-    fetch ('https://open.exchangerate-api.com/v6/latest')
+    fetch ('https://v6.exchangerate-api.com/v6/05b4701c0b67be6233177c93/latest/USD')
      .then(res => res.json()) 
      .then(res=> {
-         const units = Object.entries(res.rates);
+         const units = Object.entries(res.conversion_rates);
          console.log(units);
+         const container = document.querySelector('.container');
+         const BtnDiv = document.createElement('div');
+         BtnDiv.setAttribute('class', 'row');
+         container.appendChild(BtnDiv);
+         
          for (const [key,value] of units) 
              {
              let el = document.createElement('button');
              el.innerHTML = key;
              el.name = key;
              el.value = value;
-             el.setAttribute('class', 'button');
+             el.setAttribute('class', 'btn btn-dark');
              
              el.addEventListener('click', convert);
-             document.body.appendChild(el);
+             BtnDiv.appendChild(el);
              /* console.log(el) */
              }         
      })
@@ -31,7 +38,7 @@ const buttons = () => {
      if (!isNaN(fromAmount)) {
         document.querySelector('#toAmount').value = (fromAmount * unitValue).toFixed(2);
      } else {
-        error.innerHTML = 'Please enter a numeric value';
+        error.innerHTML = 'Please enter a numeric value.';
      }
      
 }
